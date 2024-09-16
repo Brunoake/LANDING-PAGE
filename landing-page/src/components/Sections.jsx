462a3b45ce52d4c0e7e3c72c53fb2f3cc8dce1ba
Brunoake <brunopachecogre@gmail.com>
2024-09-16 20:23:53 -0300

    .

--- landing-page/src/components/Sections.jsx
+++ landing-page/src/components/Sections.jsx
@@ -6,11 +6,13 @@ const Sections = () => {
   return (
     <div className='sections'>
         <div className="section-1">
-        <h2>Professional IT Services & Technology Consulting</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore deserunt saepe amet voluptatem ex eum officia? Dicta rem exercitationem optio quaerat!</p>
-        bitt
+        <div className='right'><h2>Professional IT Services & Technology Consulting</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore deserunt saepe amet voluptatem ex eum officia? Dicta rem exercitationem optio quaerat!</p> 
         <button>LEARN MORE</button>
-        <img src={ti} alt="" />
+        </div>
+       <div className='left'> <img src={ti} alt="" />
+       </div>
+        
         </div>
     </div>
   )
