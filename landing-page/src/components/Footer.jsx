67fac3250f1166368ecfeb1e59478b03bfb4789b
Brunoake <brunopachecogre@gmail.com>
2024-09-23 19:04:24 -0300

    .

--- landing-page/src/components/Footer.jsx
+++ landing-page/src/components/Footer.jsx
@@ -13,12 +13,24 @@ const Footer = () => {
         </div>
         <h2>We're Here To Help</h2>
         <form className="form-container">
-            <input type="text" placeholder="name"
-             required/>
-             <input type="Email" placeholder="Email Address" />
-             <input type="text" placeholder="Address"/>
-             <input type="text" placeholder="Residential/Business"/>
-
+           <div className="input-group">
+            <input type="text"required className="input"/>
+             <label className="placeholder">Name</label>
+             </div> 
+             <div className="input-group
+             "> <input type="Email"  className="input"/>
+             <label className="placeholder">Email</label>
+             </div> 
+             <div className="input-group
+             ">
+             <input type="text" className="input"/>
+             <label className="placeholder">Address</label>
+             </div> 
+             <div className="input-group
+             ">
+             <input type="text"  className="input resi"/>
+             <label className="placeholder">Resiidential/Business</label>
+             </div>
              <textarea placeholder="Message"></textarea>
 
              <button>SEND MESSAGES</button>
@@ -27,12 +39,13 @@ const Footer = () => {
           <h3><i><CiLocationOn/></i>Office</h3>
           <p>1235 Divi St.#1000 San Francisco, CA 94143</p>
 
-          <h3><CiClock2/>Hours</h3>
+          <h3><i><CiClock2/></i>Hours</h3>
           <p>M-F:8am - 10pm S-S: Closed</p>
 
-          <h3><FaPhoneAlt/>Call Us</h3>
+          <h3><i><FaPhoneAlt/></i>Call Us</h3>
           <p>(235) 396-0987</p>
         </div>
+        <h4>&#169; 2024 Brunoake</h4>
     </div>
   )
 }
